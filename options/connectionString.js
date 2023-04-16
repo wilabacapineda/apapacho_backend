471f2bc1a7f60065bfe566a1bfc67cfdcf12f013
Walter Pineda Ilabaca <walterio.pineda@gmail.com>
2023-04-16 14:15:08 -0400

    Entrega Final

--- options/connectionString.js
+++ options/connectionString.js
@@ -1,7 +1,7 @@
-import { dbType } from './optionsInit.js'
+import { dbHost } from './optionsInit.js'
 import dotenv from 'dotenv'
 dotenv.config()
 
-export const connectionStringUrl = dbType === "atlas" ?  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}/${process.env.MONGO_DBNAME}?retryWrites=true&w=majority` : `mongodb://localhost:27017/${process.env.MONGO_DBNAME}?retryWrites=true&w=majority`
-export const connectionStringUrlSessions = dbType === "atlas" ? `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}/${process.env.MONGO_DBNAMESESSIONS}?retryWrites=true&w=majority` : `mongodb://localhost:27017/${process.env.MONGO_DBNAMESESSIONS}?retryWrites=true&w=majority`
+export const connectionStringUrl = dbHost === "atlas" ?  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}/${process.env.MONGO_DBNAME}?retryWrites=true&w=majority` : `mongodb://localhost:27017/${process.env.MONGO_DBNAME}?retryWrites=true&w=majority`
+export const connectionStringUrlSessions = dbHost === "atlas" ? `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}/${process.env.MONGO_DBNAMESESSIONS}?retryWrites=true&w=majority` : `mongodb://localhost:27017/${process.env.MONGO_DBNAMESESSIONS}?retryWrites=true&w=majority`
 export default connectionStringUrl
